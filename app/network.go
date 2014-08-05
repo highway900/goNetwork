1aef7343f1139756e0fcd8bc1bbea38d276081ae
highway900 <highway900@gmail.com>
2014-08-05 18:39:24 +1000

    git...

--- app/network.go
+++ app/network.go
@@ -1,31 +1,27 @@
 package main
 
 import (
-    "fmt"
     "github.com/highway900/goNetwork"
 )
 
 func main() {
 
-    fmt.Println("Generating Network")
+    node1 := &goNetwork.Node{Data:1}
+    node2 := &goNetwork.Node{Data:2}
 
-    nNodes := 4
-    nEdges := 5
-    nodes := make([]*goNetwork.Node, nNodes)
-    edges := make([]*goNetwork.Edge, nEdges)
 
-    for _, edge := range edges {
-        newEdge := new(goNetwork.Edge)
-        newEdge.Value = i
+    for i := 0; i < 4; i++  {
+        node1.Connect(node2, float64(i))
     }
 
-    c := 0
-    for _, node := range nodes {
-        node = new(goNetwork.Node)
-        node.Value = c
-        node.AddEdge(newEdge)
-        c++
-    }
 
+    node1.Print()
+    node2.Print()
+
+    for _, edge := range node1.Edges {
+        edge.Node_src.Print()
+        edge.Node_dest.Print()
+    }
 
 }
+
--- node.go
+++ node.go
@@ -4,46 +4,85 @@ import (
     "fmt"
 )
 
-// Simple edge that holds a int value
+
 type Edge struct {
-    Value int
-    node1 *Node
-    node2 *Node
+    Node_src *Node
+    Node_dest *Node
+    Weight float64
 }
 
-// Simple Node
+
 type Node struct {
-   Value int
-   edges []*Edge
+    Data int
+    Edges []*Edge
 }
 
-func (this *Node) Print() {
 
-    fmt.Println("Node Value:", this.Value)
+func Dfs(node *Node, data int) *Node {
+    visited := make(map[*Node]bool)
+    for _, edge := range node.Edges {
+        if data == node.Data {
+            return node
+        }
+        _, found := visited[node]
+        if found {
+            continue
+        }
+        visited[node] = true
+        node_dest := node.GetDest(edge)
+        return Dfs(node_dest, data)
+    }
+    return node
+}
 
-    for _, edge := range this.edges {
 
-        fmt.Println("Edge Value:", edge.Value)
+func (node_src *Node) Connect(node_dest *Node, weight float64) {
 
+    edge := &Edge{
+        node_src,
+        node_dest,
+        weight,
     }
 
-}
-
-func (this *Node) AddEdge(edge *Edge) {
-
-   this.edges = append(this.edges, edge)
-
-}
-
-func (this *Edge) AddNodes(node1 *Node, node2 *Node) {
-
-    this.node1 = node1
-    this.node2 = node2
+    // Allocating before might be a better way
+    // of handling this. A good test for profiling.
+    node_src.Edges = append(node_src.Edges, edge)
+    node_dest.Edges = append(node_dest.Edges, edge)
 
 }
 
-func (this *Edge) PrintValue() {
 
-    fmt.Println(this.Value)
+func (node *Node) Print() {
+    fmt.Println("Data:", node.Data)
+    for i, edge := range node.Edges {
+        fmt.Printf("Edge Weight %d: %f\n", i, edge.Weight)
+    }
+}
+
+
+func MakeEdge(src *Node, dest *Node, weight float64) {
+
+    edge := &Edge{}
+    edge.Node_src = src
+    edge.Node_dest = dest
+    edge.Weight = weight
+
+}
+
+
+// This is not directed
+// Would need to use parent Graph object to handle a graph type system
+func (node *Node) GetDest(edge *Edge) *Node {
+    if node == edge.Node_dest {
+        return edge.Node_src
+    } else {
+        return edge.Node_dest
+    }
+}
+
+
+func (edge *Edge) Print() {
+
+    fmt.Println(edge.Weight)
 
 }
