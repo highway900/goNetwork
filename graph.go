e6ceeff8ca9ae3d22d7b71e8e62065802b203e63
highway900 <highway900@gmail.com>
2015-03-28 15:34:18 +1100

    not sure how old this is...

--- graph.go
+++ graph.go
@@ -1,167 +1,178 @@
 package goNetwork
 
 import (
-    "fmt"
+	"errors"
+	"fmt"
 )
 
 type SizedArray struct {
-    items []interface{}
-    current int // Array is filled to here
+	items   []interface{}
+	current int // Array is filled to here
 }
 
 func MakeSizedArray(size int32) SizedArray {
-    return SizedArray {
-        make([]interface{}, size),
-        0,
-    }
+	return SizedArray{
+		make([]interface{}, size),
+		0,
+	}
 }
 
 // returns the last item in the array
 func (sa *SizedArray) Last() interface{} {
-    return sa.items[sa.current]
+	return sa.items[sa.current]
 }
 
 // pushes the item onto the array at the back
 func (sa *SizedArray) PushBack(item interface{}) {
-    sa.items[sa.current] = item
-    sa.current += 1
+	if sa.current == len(sa.items) {
+		sa.items = append(sa.items, item)
+	} else {
+		sa.items[sa.current] = item
+	}
+	sa.current += 1
 }
 
-
 type visitMap map[*Node]bool
-
 
 // The goal of this object is to keep track of things
 // while searching etc happens
 type Visitor struct {
-    Visited visitMap
-    VisitOrder SizedArray
+	Visited    visitMap
+	VisitOrder SizedArray
 }
 
+// Mark item as visited and add to visit order
+// unless this item has already been visited
+// returns bool indicating whether this has already been
+// visited
+func (v *Visitor) Visit(i *Node) bool {
+	if _, err := v.Visited[i]; err == false {
+		v.Visited[i] = true
+		v.VisitOrder.PushBack(i)
+		return err
+	}
+	return true
+}
 
 func MakeVisitor(size int) *Visitor {
-     return &Visitor{
-        Visited: make(visitMap),
-        VisitOrder: MakeSizedArray(int32(size)),
-     }
+	return &Visitor{
+		Visited:    make(visitMap),
+		VisitOrder: MakeSizedArray(int32(size)),
+	}
 }
-
 
 type Graph struct {
-    Edges []*Edge
-    Nodes []*Node
+	Edges []*Edge
+	Nodes []*Node
 }
-
 
 type Edge struct {
-    Node_src *Node
-    Node_dest *Node
-    Weight float64
-    Key string
+	Node_src  *Node
+	Node_dest *Node
+	Weight    float64
+	Key       string
 }
-
 
 type Node struct {
-    Data int
-    Edges []*Edge
+	Data  int
+	Edges []*Edge
 }
-
 
 func (g *Graph) AddNode(data int) *Node {
-    node := &Node{Data: data}
-    g.Nodes = append(g.Nodes, node)
-    return node
+	node := &Node{Data: data}
+	g.Nodes = append(g.Nodes, node)
+	return node
 }
-
 
 func (g *Graph) Dfs(start *Node) SizedArray {
-    visitor := MakeVisitor(len(g.Nodes))
-    return g.dfs(start, visitor)
+	visitor := MakeVisitor(len(g.Nodes))
+	return g.dfs(start, visitor)
 }
-
 
 // Recursive version
 func (g *Graph) dfs(start *Node, visitor *Visitor) SizedArray {
-    visitor.Visited[start] = true
-    visitor.VisitOrder.PushBack(start)
-    for _, node := range start.OutNodes() {
-        if !visitor.Visited[node] {
-            g.dfs(node, visitor)
-        }
-    }
-    return visitor.VisitOrder
+	visitor.Visited[start] = true
+	visitor.VisitOrder.PushBack(start)
+	for _, node := range start.Connections() {
+		if !visitor.Visited[node] {
+			g.dfs(node, visitor)
+		}
+	}
+	return visitor.VisitOrder
 }
 
-
-func (g *Graph) DfsPath(start *Node, goal *Node) SizedArray {
-    visitor := MakeVisitor(len(g.Nodes)) // TODO: This is likely vBad for large networks. Consider dynamic resizing...
-    return g.dfsPath(start, goal, visitor)
+func (g *Graph) DfsPath(start *Node, goal *Node) (SizedArray, error) {
+	visitor := MakeVisitor(len(g.Nodes)) // TODO: This is likely vBad for large networks. Consider dynamic resizing...
+	return g.dfsPath(start, goal, visitor)
 }
 
-
-func (g *Graph) dfsPath(start *Node, goal *Node, visitor *Visitor) SizedArray {
-    visitor.VisitOrder.PushBack(start)
-    if start == goal {
-        return visitor.VisitOrder
-    }
-    for _, node := range start.OutNodes() {
-        if !visitor.Visited[node] {
-            g.dfsPath(start, goal, visitor)
-        }
-    }
-    return visitor.VisitOrder
+func (g *Graph) dfsPath(start *Node, goal *Node, visitor *Visitor) (SizedArray, error) {
+	visitor.Visit(start)
+	if start == goal {
+		fmt.Println("Found", visitor.VisitOrder.items)
+		return visitor.VisitOrder, nil
+	}
+	for _, node := range start.Connections() {
+		if !visitor.Visit(node) {
+			g.dfsPath(node, goal, visitor)
+		} else {
+			continue
+		}
+	}
+	// We did not find the a path to the node you are looking for
+	// return empty visitOrder and error
+	if visitor.Visit(goal) {
+		return visitor.VisitOrder, nil
+	}
+	return MakeSizedArray(0), errors.New("No Path to node found")
 }
-
 
 func (graph *Graph) Connect(node_src *Node, node_dest *Node, weight float64, key string) {
-    edge := &Edge{
-        node_src,
-        node_dest,
-        weight,
-        key,
-    }
+	edge := &Edge{
+		node_src,
+		node_dest,
+		weight,
+		key,
+	}
 
-    node_src.Edges = append(node_src.Edges, edge)
-    node_dest.Edges = append(node_dest.Edges, edge)
+	node_src.Edges = append(node_src.Edges, edge)
+	node_dest.Edges = append(node_dest.Edges, edge)
 
-    graph.Edges = append(graph.Edges, edge)
+	graph.Edges = append(graph.Edges, edge)
 }
-
 
 func (node *Node) Print(edges bool, adjacent bool) {
-    fmt.Println("Data:", node.Data)
-    for i, edge := range node.Edges {
-        if adjacent {
-            fmt.Printf("Adjacent ---> %d\n", node.GetDest(edge).Data)
-        }
-        if edges {
-            fmt.Printf("Edge %d weight: %f\n", i, edge.Weight)
-        }
-    }
-    fmt.Printf("\n")
+	fmt.Println("Data:", node.Data)
+	for i, edge := range node.Edges {
+		if adjacent {
+			fmt.Printf("Adjacent ---> %d\n", node.GetDest(edge).Data)
+		}
+		if edges {
+			fmt.Printf("Edge %d weight: %f\n", i, edge.Weight)
+		}
+	}
+	fmt.Printf("\n")
 }
 
-
-func (node *Node) OutNodes() []*Node {
-    outNodes := make([]*Node, len(node.Edges))
-    for i, edge := range node.Edges {
-        outNodes[i] = node.GetDest(edge)
-    }
-    return outNodes
+// TODO handle direction, rename to connections
+func (node *Node) Connections() []*Node {
+	outNodes := make([]*Node, len(node.Edges))
+	for i, edge := range node.Edges {
+		outNodes[i] = node.GetDest(edge)
+	}
+	return outNodes
 }
-
 
 // This is not directed
 // Would need to use parent Graph object to handle a graph type system
 func (node *Node) GetDest(edge *Edge) *Node {
-    if node == edge.Node_dest {
-        return edge.Node_src
-    } else {
-        return edge.Node_dest
-    }
+	if node == edge.Node_dest {
+		return edge.Node_src
+	} else {
+		return edge.Node_dest
+	}
 }
 
-
 func (edge *Edge) Print() {
-    fmt.Println(edge.Weight)
+	fmt.Println(edge.Weight)
 }
--- multigraph_test.go
+++ multigraph_test.go
@@ -1,93 +1,119 @@
 package goNetwork
 
-
 import (
-    "testing"
-    "fmt"
+	"fmt"
+	"testing"
 )
-
 
 var seed int64
 
+func TitleForTest(s string) {
+    fmt.Printf("\n================\nTesting: %s\n................\n\n", s)
+}
 
 func SetupTest(s string) *Graph {
 
-    fmt.Printf("\n================\nTesting: %s\n................\n\n", s)
+    TitleForTest(s)
 
-    graph := new(Graph)
+	graph := new(Graph)
 
-    // create a node
-    node1 := graph.AddNode(1)
-    node2 := graph.AddNode(2)
-    node3 := graph.AddNode(3)
-    node4 := graph.AddNode(4)
-    node5 := graph.AddNode(5)
-    node6 := graph.AddNode(6)
-    node7 := graph.AddNode(7)
+	// create a node
+	node1 := graph.AddNode(1)
+	node2 := graph.AddNode(2)
+	node3 := graph.AddNode(3)
+	node4 := graph.AddNode(4)
+	node5 := graph.AddNode(5)
+	node6 := graph.AddNode(6)
+	node7 := graph.AddNode(7)
 
-    graph.Connect(node1, node2, 1.0, "k")
-    graph.Connect(node1, node3, 1.0, "k")
+	graph.Connect(node1, node2, 1.0, "k")
+	graph.Connect(node1, node3, 1.0, "k")
 
-    graph.Connect(node2, node4, 1.0, "k")
-    graph.Connect(node3, node4, 1.0, "k")
-    graph.Connect(node5, node4, 1.0, "k")
-    graph.Connect(node1, node6, 1.0, "k")
-    graph.Connect(node6, node5, 1.0, "k")
-    graph.Connect(node6, node7, 1.0, "k")
+	graph.Connect(node2, node4, 1.0, "k")
+	graph.Connect(node3, node4, 1.0, "k")
+	graph.Connect(node5, node4, 1.0, "k")
+	graph.Connect(node1, node6, 1.0, "k")
+	graph.Connect(node6, node5, 1.0, "k")
+	graph.Connect(node6, node7, 1.0, "k")
 
-    return graph
+	return graph
 
 }
 
 func TestMultiGraphCreate(t *testing.T) {
 
-    SetupTest("Connect MultiGraph")
+	SetupTest("Connect MultiGraph")
 
-    // create a node
-    node1 := &Node{Data: 1}
-    node2 := &Node{Data: 2}
-    node3 := &Node{Data: 3}
+	// create a node
+	node1 := &Node{Data: 1}
+	node2 := &Node{Data: 2}
+	node3 := &Node{Data: 3}
 
-    graph := new(Graph)
-    graph.Connect(node1, node2, 1.0, "k")
-    graph.Connect(node1, node3, 1.0, "k")
+	graph := new(Graph)
+	graph.Connect(node1, node2, 1.0, "k")
+	graph.Connect(node1, node3, 1.0, "k")
 
-    node1.Print(true, true)
+	node1.Print(true, true)
 
 }
-
 
 func printKV(r []interface{}) {
-    for _, v := range r {
-        fmt.Println(v.(*Node).Data)
-    }
+	for _, v := range r {
+		fmt.Println(v.(*Node).Data)
+	}
 }
-
 
 func TestGraphOutNodes(t *testing.T) {
-    graph := SetupTest("OutNodes")
-    for _, node := range graph.Nodes {
-        for _, n := range node.OutNodes() {
-            fmt.Printf("%d --> %d\n", node.Data, n.Data)
-        }
-    }
+	graph := SetupTest("OutNodes")
+	for _, node := range graph.Nodes {
+		for _, n := range node.Connections() {
+			fmt.Printf("%d --> %d\n", node.Data, n.Data)
+		}
+	}
 }
 
-
 func TestDfsMultiGraph(t *testing.T) {
-    graph := SetupTest("DFS Recursive")
+	graph := SetupTest("DFS Recursive")
 
-    m := graph.Dfs(graph.Nodes[5])
-    fmt.Println("visit order items", m.items)
-    // Compare the m.items to graph.Nodes both should contain the same items.
-    should_be_empty := m.items
-    fmt.Println(should_be_empty)
-    for x, i := range m.items {
-        for _, j := range graph.Nodes {
-            if i == j {
-                should_be_empty = should_be_empty[1:]
-            }
-        }
+	m := graph.Dfs(graph.Nodes[5])
+	fmt.Println("visit order items", m.items)
+	// Compare the m.items to graph.Nodes both should contain the same items.
+	should_be_empty := m.items
+	fmt.Println(should_be_empty)
+	for _, i := range m.items {
+		for _, j := range graph.Nodes {
+			if i == j {
+				should_be_empty = should_be_empty[1:]
+			}
+		}
+	}
+	if len(should_be_empty) != 0 {
+		t.FailNow()
+	}
+}
+
+func TestDfsPathMultiGraph(t *testing.T) {
+
+    TitleForTest("Testing DFS Path")
+
+	graph := new(Graph)
+
+	// create a node
+	node1 := graph.AddNode(1)
+	node2 := graph.AddNode(2)
+	node3 := graph.AddNode(3)
+
+    graph.Connect(node1, node2, 1.0, "1")
+	graph.Connect(node2, node3, 1.0, "1")
+    graph.Connect(node1, node3, 1.0, "1")
+
+	m, err := graph.DfsPath(graph.Nodes[0], graph.Nodes[1])
+	if err != nil {
+        fmt.Println("ERROR:", err)
+		t.FailNow()
+	}
+
+    for _, i := range m.items {
+        fmt.Println(i.(*Node).Data)
     }
-    fmt.Println(should_be_empty)
 }
